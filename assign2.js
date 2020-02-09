259e51f77e914107d993be60911b7b0d16f867bc
NicolasRoss <g.nicolas.ross@gmail.com>
2020-02-08 20:27:48 -0500

    Assignment 2

--- assign2.js
+++ assign2.js
@@ -1,24 +1,70 @@
-function hover(img) {
-    document.getElementById(img).style.transform = "scale(1.2, 1.2)";
+function q1() {
+    var similar = document.querySelectorAll(".section.similar");
+    // console.log(similar);
+    for (i = 0; i < similar.length; i++) {
+        var img = similar[i].querySelector("img");
+        img.addEventListener("mouseover", function(event) {
+            event.target.style.transform = "scale(1.2, 1.2)";
+        });
+
+        img.addEventListener("mouseout", function(event) {
+            event.target.style.transform = "scale(1, 1)";
+        });
+    }
 }
 
-function unhover(img) {
-    document.getElementById(img).style.transform = "scale(1, 1)";
-}
+function q2() {
+    var form = document.querySelector("#signUpForm");
+    var selector = "input[type=text],input[type=password],input[type=email],input[type=tel]";
+    var fields = document.querySelectorAll(selector);
+    // console.log(fields);
+    form.addEventListener("submit", function(event) {
+        for (i = 0; i < fields.length; i++) {
+            if(fields[i].value == "") {
+                fields[i].style.backgroundColor = "#ffcccc";
+                fields[i].style.borderColor = "red";
+                fields[i].style.backgroundImage = "url('images/error.png')";
+                fields[i].style.backgroundRepeat = "no-repeat";
+                fields[i].style.backgroundPosition = "right";
 
-function isValid() {
-    var form = document.getElementById("signUpForm").elements;
-    
-    for (var i = 0; i < form.length; i++) {
-        if (form[i].value == "" || form[i].checked == false) {
-            form[i].style.borderColor = "red";
-            form[i].style.bacvground = "#ffcccc";
+            } else {
+                fields[i].style.backgroundColor = null;
+                fields[i].style.borderColor = null;
+                fields[i].style.backgroundImage = null;
+            }
+        }
+
+        var conts = document.querySelectorAll("input[name='Continent']");
+        var checked = false;
+
+        for (i = 0; i < conts.length; i++) {
+            if (conts[i].checked) {
+                checked = true;
+                break;
+            }
+        }
+
+        var continents = document.querySelector(".continent");
+
+        if (!checked) {
+            continents.style.backgroundColor = "#ffcccc";
+            continents.style.backgroundImage = "url('images/error.png')";
+            continents.style.backgroundRepeat = "no-repeat";
+            continents.style.backgroundPosition = "right";
 
         } else {
-            form[i].style.borderColor = "";
-            form[i].style.bacvground = "";
+            continents.style.backgroundColor = null;
+            continents.style.backgroundImage = null;
         }
-    }
 
-    return false;
-}
+        var agreement = document.querySelector(".terms")
+        var check = document.querySelector(".agreement")
+        if(!check.checked) {
+            agreement.style.backgroundColor = "#ffcccc";
+            alert("Please agree to the terms and services.");
+
+        } else {
+            agreement.style.backgroundColor = null;
+        }
+    });
+} 
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Name: Nicolas Ross
+ID: 151703880
+Email: ross3880@mylaurier.ca
+Assignment_ID: CP476A2
+Homework statement: I claim that the enclosed submission is myindividual work.
